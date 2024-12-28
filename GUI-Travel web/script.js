b1f72cee872eb77162ca06a724ac58c8eb802b8c
Sarasi Madahasi <183228383+Sarasii22@users.noreply.github.com>
2024-12-28 22:48:49 +0530

    home page 1

---
+++ GUI-Travel web/script.js
@@ -0,0 +1,11 @@
+
+/*login button*/
+document.querySelector('.login-btn').addEventListener('click', () => {
+  alert('Login functionality coming soon!');
+});
+
+
+/*explore button*/
+document.querySelector('.explore-btn').addEventListener('click', () => {
+  alert('Explore section is under construction.');
+});
